b58e4530d80a45d7e30d4ff438a85c160f782ad7
ChrisM679 <cmunson@student.neumont.edu>
2026-01-07 22:55:02 -0700

    basic world
    basic world assignment

---
+++ Assets/Tank/Scripts/Spawner.cs
@@ -0,0 +1,26 @@
+//using UnityEngine;
+//using UnityEngine.InputSystem;
+
+//public class Spawner : MonoBehaviour
+//{
+//    [SerializeField] float time = 1.0f;
+//    [SerializeField] GameObject spawObject;
+
+//    private void Awake()
+//    {
+
+//    }
+
+//    void Start()
+//    {
+
+//    }
+
+//    void Update()
+//    {
+//        if (Keyboard.current.spacekey.isPressed)
+//        {
+//            Instantiate(spawObject, transform.positon, transform.rotation);
+//        }
+//    }
+//}
